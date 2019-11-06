a11ed24bc17d97691a00eee6ac6d41b508f16c9e
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-06 14:43:01 -0500

    Update PhoneBook.h

--- PhoneBook.h
+++ PhoneBook.h
@@ -31,7 +31,7 @@ public:
     vector<PhoneEntry> findByFirst(string first);
     vector<PhoneEntry> findByLast(string last);
     vector<PhoneEntry> findByType(string type);
-    void addEntry(string id, string first,string last,string phone,string type);
+    void addEntry(string first,string last,string phone,string type);
     void editEntry(string idnum,string first,string last,string phone,string type);
     void deleteEntry(string idnum);
 private:
