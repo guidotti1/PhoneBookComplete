4d64346e1e417595e97424495438e6816ab181c7
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-06 14:55:45 -0500

    Update PhoneEntry.cpp

--- PhoneEntry.cpp
+++ PhoneEntry.cpp
@@ -4,10 +4,9 @@
 PhoneEntry::PhoneEntry() {
 }
 
-PhoneEntry::PhoneEntry(string f, string l, string p, string t, string I) {
+PhoneEntry::PhoneEntry(string f, string l, string p, string t) {
     first = f;
     last = l;
     phone = p;
     type = t;
-    ID=I;
 }
